b61943498cf48c6e3787ade1db5170cd31a314c6
Ketchup <71892410+KetchupKing@users.noreply.github.com>
2024-04-20 22:14:06 +0100

    update

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ app.secret_key = os.getenv('TOKEN')
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
-
 logs = []
+
+
 def load_logs():
     global logs
     logs = []
@@ -24,28 +25,37 @@ def load_logs():
             except json.JSONDecodeError:
                 print(f"Error parsing line: {line}")
 
-load_logs()
+
+def load_credentials():
+    with open('credentials.json', 'r') as file:
+        credentials = json.load(file)
+    return credentials['username'], credentials['password']
+
 
 class User(UserMixin):
     def __init__(self, id):
         self.id = id
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User(user_id)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == 'admin' and password == 'password':#REPLACE WITH DIFFERENT CREDENTIALS
+        stored_username, stored_password = load_credentials()
+        if username == stored_username and password == stored_password:
             user = User(1)
             login_user(user)
             return redirect(url_for('index'))
         else:
             return 'Invalid credentials'
     return render_template('login.html')
+
 
 @app.route('/logout')
 @login_required
@@ -54,11 +64,13 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 @app.route('/')
 @login_required
 def index():
     load_logs()
     return render_template('index.html')
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -69,11 +81,13 @@ def search():
             results.append(log)
     return jsonify(results)
 
+
 @app.route('/filters', methods=['GET'])
 def get_filters():
     command_names = set(log['command_name'] for log in logs)
     user_names = set(log['user_name'] for log in logs)
     return jsonify({'command_names': list(command_names), 'user_names': list(user_names)})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
