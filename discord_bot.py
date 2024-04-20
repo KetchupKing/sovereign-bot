6e977eae63ba0fffa7aa830949fc45861c559e08
Ketchup <71892410+KetchupKing@users.noreply.github.com>
2024-04-20 14:43:49 +0100

    account maker
    command to make a personal account of everyone in the discord server

--- discord_bot.py
+++ discord_bot.py
@@ -20,8 +20,11 @@ ACCOUNTS_DATA_DIR = os.path.join(os.path.dirname(__file__), 'accounts_data')
 COMPANY_DATA_DIR = os.path.join(os.path.dirname(__file__), 'company_data')
 os.makedirs(ACCOUNTS_DATA_DIR, exist_ok=True)
 os.makedirs(COMPANY_DATA_DIR, exist_ok=True)
+
 intents = discord.Intents.default()
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
@@ -669,6 +672,37 @@ async def eco(
         await ctx.respond(recipient_accounts)
     else:
         await ctx.respond("You are not admin")
+
+
+@bot.slash_command(name="account_all", description="Create a personal account for everyone in the server.")
+async def account_all(
+    ctx,
+    ephemeral: bool = discord.Option(bool, description="Make the response ephemeral", required=False, default=False)
+):
+    log_event(ctx.author.id, ctx.author.name, "account_all", {"ephemeral": ephemeral})
+    if user_id in admin:
+        members = await ctx.guild.fetch_members().flatten()
+        for member in members:
+            user_id = str(member.id)
+            userName = str(member.name)
+            accounts = load_accounts(user_id)
+            if not accounts:
+                accounts = {
+                    "personal": {
+                        "account_name": userName,
+                        "balance": 1000,
+                        "currency": "Sovereign",
+                        "own accounts": [],
+                        "treasurer of": []
+                    }
+                }
+                save_accounts(user_id, accounts)
+                print(f"A new personal account has been created for {userName}.")
+            else:
+                print(f"{userName} already has an account.")
+        await ctx.respond("All personal accounts have been created.", ephemeral=ephemeral)
+    else:
+        await ctx.respond("You do not have permission to use this command.", ephemeral=ephemeral)
 
 
 bot.run(TOKEN)
